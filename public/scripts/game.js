6631f0b109562e9bf6b16110d75d6e0f80f57045
Morgan Laco <morgan@codeschool.com>
2016-05-13 23:28:55 -0400

    More positioning of hands

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,11 +1,11 @@
 var aiChoice;
-$(document) .ready(function() {
+$(document).ready(function() {
   aiChoice = Math.floor(Math.random() * 3);
   if (aiChoice==0) {
-    $(".fa-hand-rock-o").toggleClass("is-hidden");
+    $("#ai-rock").toggleClass("is-hidden");
   } else if (aiChoice==1) {
-    $(".fa-hand-paper-o").toggleClass("is-hidden");
+    $("#ai-paper").toggleClass("is-hidden");
   } else if (aiChoice==2) {
-    $(".fa-hand-scissors-o").toggleClass("is-hidden");
+    $("#ai-scissors").toggleClass("is-hidden");
   }
 });
