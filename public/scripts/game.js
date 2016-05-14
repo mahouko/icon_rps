5820decb77caee4c7c015731e99052fbec3d40f7
jocelyn <mahouko8@gmail.com>
2016-05-14 17:20:38 -0400

    working on ai choice refactor

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -4,12 +4,15 @@ $(document).ready(function() {
     $(".ai-hand").addClass("is-hidden");
 
     aiChoice = Math.floor(Math.random() * 3);
+    var rock ="<i id=\"ai-rock\" class=\"fa fa-hand-rock-o fa-5x rotate-270-flip ai-hand\" aria-hidden=\"true\"></i>"
+    var paper ="<i id=\"ai-paper\" class=\"fa fa-hand-paper-o fa-5x rotate-270-flip ai-hand\" aria-hidden=\"true\"></i>"
+    var scissors ="<i id=\"ai-scissors\" class=\"fa fa-hand-scissors-o fa-5x ai-hand is-hidden\" aria-hidden=\"true\"></i>"
     if (aiChoice==0) {
-      $("#ai-rock").toggleClass("is-hidden");
+      $("#ai-choice").html(rock);
     } else if (aiChoice==1) {
-      $("#ai-paper").toggleClass("is-hidden");
+      $("#ai-choice").html(paper);
     } else if (aiChoice==2) {
-      $("#ai-scissors").toggleClass("is-hidden");
+      $("#ai-choice").html(scissors);
     }
   }
 
