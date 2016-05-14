4f63cd6f81cf8deedcbbd3451d9d50439c6a9dd5
jocelyn <mahouko8@gmail.com>
2016-05-14 17:59:22 -0400

    implemented player input

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
   var rock ="<i id=\"ai-rock\" class=\"fa fa-hand-rock-o fa-5x rotate-270-flip ai-hand\" aria-hidden=\"true\"></i>"
   var paper ="<i id=\"ai-paper\" class=\"fa fa-hand-paper-o fa-5x rotate-270-flip ai-hand\" aria-hidden=\"true\"></i>"
   var scissors ="<i id=\"ai-scissors\" class=\"fa fa-hand-scissors-o fa-5x ai-hand \" aria-hidden=\"true\"></i>"
-  
+  var playerRock ="<i id=\"input-rock\" class=\"fa fa-hand-rock-o fa-5x fa-rotate-90 player-hand\" aria-hidden=\"true\"></i>"
+  var playerPaper ="<i id=\"input-paper\" class=\"fa fa-hand-paper-o fa-5x fa-rotate-90 player-hand\" aria-hidden=\"true\"></i>"
+  var playerScissors ="<i id=\"input-scissors\" class=\"fa fa-hand-scissors-o fa-5x rotate-180-flip player-hand\" aria-hidden=\"true\"></i>"
+
   function makeAiChoice(){
     aiChoice = Math.floor(Math.random() * 3);
     if (aiChoice==0) {
@@ -15,8 +18,26 @@ $(document).ready(function() {
     }
   }
 
-  $(".player-hand").click(function(){
-    // $("#game").html("<i id=\"ai-paper\" class=\"fa fa-hand-paper-o fa-5x rotate-270-flip ai-hand is-hidden\" aria-hidden=\"true\"></i>")
+  function displayPlayerChoice(playerChoice){
+    if (playerChoice==0) {
+      $("#player-choice").html(playerRock);
+    } else if (playerChoice==1) {
+      $("#player-choice").html(playerPaper);
+    } else if (playerChoice==2) {
+      $("#player-choice").html(playerScissors);
+    }
+  }
+
+  $("#input-rock").click(function(){
+    displayPlayerChoice(0)
+    makeAiChoice();
+  });
+  $("#input-paper").click(function(){
+    displayPlayerChoice(1)
+    makeAiChoice();
+  });
+  $("#input-scissors").click(function(){
+    displayPlayerChoice(2)
     makeAiChoice();
   });
 });
