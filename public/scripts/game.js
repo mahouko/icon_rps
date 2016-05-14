7cfdada905a67573548d0b1868ec71188c3a7fbc
jocelyn <mahouko8@gmail.com>
2016-05-14 17:22:43 -0400

    removed ishidden from ai scissors

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     aiChoice = Math.floor(Math.random() * 3);
     var rock ="<i id=\"ai-rock\" class=\"fa fa-hand-rock-o fa-5x rotate-270-flip ai-hand\" aria-hidden=\"true\"></i>"
     var paper ="<i id=\"ai-paper\" class=\"fa fa-hand-paper-o fa-5x rotate-270-flip ai-hand\" aria-hidden=\"true\"></i>"
-    var scissors ="<i id=\"ai-scissors\" class=\"fa fa-hand-scissors-o fa-5x ai-hand is-hidden\" aria-hidden=\"true\"></i>"
+    var scissors ="<i id=\"ai-scissors\" class=\"fa fa-hand-scissors-o fa-5x ai-hand \" aria-hidden=\"true\"></i>"
     if (aiChoice==0) {
       $("#ai-choice").html(rock);
     } else if (aiChoice==1) {
