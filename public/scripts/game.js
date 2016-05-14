ef478735cac8d52ba624b62d28f9ccd590876511
jocelyn <mahouko8@gmail.com>
2016-05-14 15:54:12 -0400

    enabled both player and computer results

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,11 +1,19 @@
 var aiChoice;
 $(document).ready(function() {
-  aiChoice = Math.floor(Math.random() * 3);
-  if (aiChoice==0) {
-    $("#ai-rock").toggleClass("is-hidden");
-  } else if (aiChoice==1) {
-    $("#ai-paper").toggleClass("is-hidden");
-  } else if (aiChoice==2) {
-    $("#ai-scissors").toggleClass("is-hidden");
+  function makeAiChoice(){
+    $(".ai-hand").addClass("is-hidden");
+
+    aiChoice = Math.floor(Math.random() * 3);
+    if (aiChoice==0) {
+      $("#ai-rock").toggleClass("is-hidden");
+    } else if (aiChoice==1) {
+      $("#ai-paper").toggleClass("is-hidden");
+    } else if (aiChoice==2) {
+      $("#ai-scissors").toggleClass("is-hidden");
+    }
   }
+
+  $(".player-hand").click(function(){
+    makeAiChoice();
+  });
 });
