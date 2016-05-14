c96ed282b925ed57e3c835b64760acce3d38b355
Morgan Laco <morgan@codeschool.com>
2016-05-14 17:26:02 -0400

    Little refactor

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,12 +1,11 @@
 var aiChoice;
 $(document).ready(function() {
+  var rock ="<i id=\"ai-rock\" class=\"fa fa-hand-rock-o fa-5x rotate-270-flip ai-hand\" aria-hidden=\"true\"></i>"
+  var paper ="<i id=\"ai-paper\" class=\"fa fa-hand-paper-o fa-5x rotate-270-flip ai-hand\" aria-hidden=\"true\"></i>"
+  var scissors ="<i id=\"ai-scissors\" class=\"fa fa-hand-scissors-o fa-5x ai-hand \" aria-hidden=\"true\"></i>"
+  
   function makeAiChoice(){
-    $(".ai-hand").addClass("is-hidden");
-
     aiChoice = Math.floor(Math.random() * 3);
-    var rock ="<i id=\"ai-rock\" class=\"fa fa-hand-rock-o fa-5x rotate-270-flip ai-hand\" aria-hidden=\"true\"></i>"
-    var paper ="<i id=\"ai-paper\" class=\"fa fa-hand-paper-o fa-5x rotate-270-flip ai-hand\" aria-hidden=\"true\"></i>"
-    var scissors ="<i id=\"ai-scissors\" class=\"fa fa-hand-scissors-o fa-5x ai-hand \" aria-hidden=\"true\"></i>"
     if (aiChoice==0) {
       $("#ai-choice").html(rock);
     } else if (aiChoice==1) {
